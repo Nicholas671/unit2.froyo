a88be6bd23f3e17c88963e1bed28a72f0423b5ff
Nicholas671 <nicholas.a.crabtree@gmail.com>
2025-01-07 14:43:03 -0700

    array created

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const inputFlavor = prompt("Please enter flavors separated by commas.", "coffee, coffee, vanilla, vanilla,vanilla, strawberry");
+const inputFlavor = prompt("Please enter the Froyo flavors once per each that you want, separated by commas.");
+const flavorArray = inputFlavor.split(",");
+
+
+
+const result = countFlavors(flavorArray);
+
+
+console.log(flavorArray);
+console.log(result);
+
+
+
+
+
+
+
+
+
